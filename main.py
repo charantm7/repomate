6443ae64d82b73351d6da575c22740da464954da
charantm <charantm8787@gmail.com>
2025-06-25 13:16:17 +0530

    updated

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from enum import Enum
-
+from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
+
+class Student(BaseModel):
+    name:str
+    age:int | None = None
+    sem:str
+    course:str
+    college:str
 
 
 app = FastAPI()
@@ -21,3 +29,26 @@ async def get_model(name: ModelName):
         return {"name": name, "message": "LeCNN all the images"}
     else:
         return {"name": name, "message": "Have some residuals"}
+    
+@app.get("/price/{item_id}")
+async def price(item_id:int, price:int, quantity:int=0, available: Union[bool, None] = None):
+
+    item = {
+        "Item ID":item_id,
+        "Price":price,
+        "Quantity":quantity,
+        "Item Available":available
+    }
+    return item
+
+@app.put("/student/{item_id}")
+async def create_student(item_id:int, student:Student, passed:Union[bool, None] = None):
+    result = {"item_id":item_id, **student.dict()}
+    if passed is True:
+        result.update({"Result": f"{student.name} has passed in {student.sem} sem"})
+
+    else:
+        result.update({"Result": f"{student.name} has Failed in {student.sem} Sem"})
+
+
+    return result
